98d7a0f7efd63391526fca12a8f426bb3c861866
mattrothenberg <mattrothenberg@gmail.com>
2015-05-01 07:55:23 -0400

    add party affiliation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 		.done(function(vis, layers) {
 			layers[1].on('featureClick', function(e, latlng, pos, data) {
 				console.log(this);
+				$('.rep-party').removeClass("republican").removeClass("democrat");
 				$('.rubber_stamp').remove();
 
 				var district_id = data.cartodb_id;
@@ -13,8 +14,6 @@ $(document).ready(function() {
 				var appPlaceholderMessage= $('.app-placeholder');
 				var district_information_container = $('.district-information');
 				var district_id_display = $(".district-id");
-
-				console.log(this);
 
 				// Hide your kids, hide your instructions
 				$(appInstructionDiv).addClass("hide");
@@ -28,14 +27,24 @@ $(document).ready(function() {
 
 				$.getJSON('https://'+ 'mrothenberg' +'.cartodb.com/api/v2/sql/?q='+ 'SELECT representative from georgia_senate_merge where cartodb_id = ' + district_id, function(data) {
 					var repName = data.rows[0].representative;
-					var parsed = NameParse.parse(repName);
-					console.log(parsed);
-					$('.rep-name').html(repName);
 
 					$.getJSON('http://openstates.org/api/v1/legislators/?apikey=ba9b8faa7fa14a52b22569c402b975c6&state=ga&chamber=upper&district=' + district_id, function(data) {
 						$('.rep-img').attr("src", data[0].photo_url);
-						$('.rep-figure').append('<div class="rubber_stamp animated bounceInDown">Unopposed</div>');
+
+						if ( data[0].party == "Democratic" ) {
+							$('.rep-party').addClass("democrat");
+						} else if ( data[0].party == "Republican" ) {
+							$('.rep-party').addClass("republican");
+						} else {}
+
+						$('.rep-name').html(data[0].full_name);
+						$('.district-information').append('<div class="rubber_stamp animated bounceInDown">Unopposed</div>');
+
+						// $.getJSON('https://transparencydata.org/api/1.0/entities.json?apikey=ba9b8faa7fa14a52b22569c402b975c6&search=' + data[0].full_name  + '&type=politician', function(data) {
+						// 	console.log(data);
+						// })
 					})
+
 				});
 
 			})
